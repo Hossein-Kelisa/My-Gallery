3fbb8e4c5958ec4e2b4c5fcbcbd3fa4dfaba7498
Hossein <Hossein.kelisa@gmail.com>
2025-09-24 14:34:08 +0200

    initilize app

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,13 @@
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import img7 from "./images/img7.jpg";
+import img8 from "./images/img8.jpg";
+import vid1 from "./videos/vid1.mp4";
+import vid2 from "./videos/vid2.mp4";
 import "./App.css";
 
 function App() {
@@ -27,27 +37,29 @@ function App() {
         <section id="photos" className="photos-section">
           <h1>Photo Album</h1>
           <div className="photo-grid">
-            <img src="https://via.placeholder.com/150" alt="Sample 1" />
-            <img src="https://via.placeholder.com/150" alt="Sample 2" />
-            <img src="https://via.placeholder.com/150" alt="Sample 3" />
-            <img src="https://via.placeholder.com/150" alt="Sample 4" />
-            <img src="https://via.placeholder.com/150" alt="Sample 5" />
-            <img src="https://via.placeholder.com/150" alt="Sample 6" />
+            <img src={img1} alt="Sample 1" />
+            <img src={img2} alt="Sample 2" />
+            <img src={img3} alt="Sample 3" />
+            <img src={img4} alt="Sample 4" />
+            <img src={img5} alt="Sample 5" />
+            <img src={img6} alt="Sample 6" />
+            <img src={img7} alt="Sample 7" />
+            <img src={img8} alt="Sample 8" />
           </div>
         </section>
         <section id="videos" className="videos-section">
           <h1>Video Gallery</h1>
           <div className="video-grid">
-            <video controls>
+            <video controls >
               <source
-                src="https://www.w3schools.com/html/mov_bbb.mp4"
+                src={vid1}
                 type="video/mp4"
               />
               Your browser does not support the video tag.
             </video>
             <video controls>
               <source
-                src="https://www.w3schools.com/html/mov_bbb.mp4"
+                src={vid2}
                 type="video/mp4"
               />
               Your browser does not support the video tag.
